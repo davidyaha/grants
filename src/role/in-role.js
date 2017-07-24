8e4d6cb0c5970c324cb97b4c0fd6f2a38cf8f06d
David Yahalomi <davidyaha@gmail.com>
2017-07-24 19:27:49 +0300

    Fixed all flow errors

--- src/role/in-role.js
+++ src/role/in-role.js
@@ -4,6 +4,8 @@ import { Role } from './role';
 import { getRoleByName } from './get';
 import { compose } from '../utils';
 
+import type { Map } from 'immutable';
+
 const isInRole = (role: Role) => (member: any) => !Role.isEmpty(role) && role.members.has(member);
 
 export const inRole = (roles: Map<string, Role>) => compose(
